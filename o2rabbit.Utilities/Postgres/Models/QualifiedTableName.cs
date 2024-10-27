ee9b2b6fe7e20bb3c4597eecb1b49db48c76d622
felix <haller.felix@outlook.de>
2024-10-27 07:14:38 +0100

    Add QualifiedTableName.cs

---
+++ o2rabbit.Utilities/Postgres/Models/QualifiedTableName.cs
@@ -0,0 +1,44 @@
+namespace o2rabbit.Utilities.Postgres.Models;
+
+public class QualifiedTableName
+{
+    private string _table = null!;
+    private string _schema = null!;
+    
+    public QualifiedTableName(string schema, string table)
+    {
+        Schema = schema;
+        Table = table;
+    }
+
+    public required string Table
+    {
+        get => _table;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"'{nameof(value)}' cannot be null or whitespace.", nameof(value));
+            }
+
+            _table = value;
+        }
+    }
+
+
+    public required string Schema
+    {
+        get => _schema;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"'{nameof(value)}' cannot be null or whitespace.", nameof(value));
+            }
+
+            _schema = value;
+        }
+    }
+
+    public override string ToString() => $"[{Schema}].[{Table}]";
+}
