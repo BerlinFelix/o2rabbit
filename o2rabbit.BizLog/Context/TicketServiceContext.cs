6d2a354b4d58579aeccc01049803ec35c0897b59
felix <haller.felix@outlook.de>
2024-11-10 12:23:33 +0100

    Add entity configurations to TicketServiceContext
    Integrated entity configurations for `Process` and `Ticket` models in `TicketServiceContext`. This includes table mappings, primary key definitions, auto-generation for Id properties, and configuration for parent-child relationships with optional foreign keys.

--- o2rabbit.BizLog/Context/TicketServiceContext.cs
+++ o2rabbit.BizLog/Context/TicketServiceContext.cs
@@ -29,4 +29,37 @@ public class TicketServiceContext : DbContext
     {
         optionsBuilder.UseNpgsql(_options.Value.ConnectionString);
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Process>()
+            .ToTable("Processes")
+            .HasKey(x => x.Id);
+
+        modelBuilder.Entity<Process>()
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Process>()
+            .HasMany(x => x.Children)
+            .WithOne(x => x.Parent)
+            .HasForeignKey(x => x.ParentId)
+            .IsRequired(false);
+
+        modelBuilder.Entity<Ticket>()
+            .ToTable("Tickets")
+            .HasKey(x => x.Id);
+
+        modelBuilder.Entity<Ticket>()
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Ticket>()
+            .HasMany(x => x.Children)
+            .WithOne(x => x.Parent)
+            .HasForeignKey(x => x.ParentId)
+            .IsRequired(false);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
