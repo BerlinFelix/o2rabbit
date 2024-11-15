827d29945e4369a3a072c7db49fc2e49cfb3c42a
felix <haller.felix@outlook.de>
2024-11-15 19:04:44 +0100

    Add ITicketValidator mock to DeleteAsync tests
    Introduced a mock of ITicketValidator in the DeleteAsync test class to enhance the testing of the TicketService delete functionality.

--- o2rabbit.BizLog.Tests/Services/WhenUsingTicketService/DeleteAsync.cs
+++ o2rabbit.BizLog.Tests/Services/WhenUsingTicketService/DeleteAsync.cs
@@ -1,109 +1,111 @@
-// using AutoFixture;
-// using FluentAssertions;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.Extensions.Logging;
-// using Microsoft.Extensions.Options;
-// using Moq;
-// using o2rabbit.BizLog.Context;
-// using o2rabbit.BizLog.Options.TicketServiceContext;
-// using o2rabbit.BizLog.Services;
-// using o2rabbit.BizLog.Tests.AutoFixtureCustomization;
-// using o2rabbit.Core.Entities;
-// using o2rabbit.Core.ResultErrors;
-// using o2rabbit.Migrations.Context;
-//
-// namespace o2rabbit.BizLog.Tests.Services.WhenUsingTicketService;
-//
-// public class DeleteAsync : IAsyncLifetime, IClassFixture<TicketServiceClassFixture>
-// {
-//     private readonly TicketServiceClassFixture _classFixture;
-//     private readonly DefaultContext _defaultContext;
-//     private readonly Fixture _fixture;
-//     private readonly TicketService _sut;
-//     private readonly TicketServiceContext _ticketContext;
-//
-//     public DeleteAsync(TicketServiceClassFixture classFixture)
-//     {
-//         _classFixture = classFixture;
-//         _defaultContext = new DefaultContext(_classFixture.ConnectionString);
-//         _fixture = new Fixture();
-//         _fixture.Customize(new TicketHasNoProcessNoParentsNoChildren());
-//
-//         _ticketContext =
-//             new TicketServiceContext(
-//                 new OptionsWrapper<TicketServiceContextOptions>(new TicketServiceContextOptions()
-//                 {
-//                     ConnectionString = _classFixture.ConnectionString!
-//                 }));
-//
-//         var loggerMock = new Mock<ILogger<TicketService>>();
-//         _sut = new TicketService(_ticketContext, loggerMock.Object);
-//     }
-//
-//     public async Task InitializeAsync()
-//     {
-//         var migrationContext = new DefaultContext(_classFixture.ConnectionString);
-//         await migrationContext.Database.EnsureCreatedAsync();
-//
-//         var existingTicket = _fixture.Create<Ticket>();
-//         var existingTicket2 = _fixture.Create<Ticket>();
-//         existingTicket.Id = 1;
-//         existingTicket2.Id = 2;
-//
-//         _defaultContext.Add(existingTicket);
-//         _defaultContext.Add(existingTicket2);
-//
-//         await _defaultContext.SaveChangesAsync();
-//
-//         _defaultContext.Entry(existingTicket).State = EntityState.Detached;
-//         _defaultContext.Entry(existingTicket2).State = EntityState.Detached;
-//     }
-//
-//     [Theory]
-//     [InlineData(-1)]
-//     [InlineData(5)]
-//     public async Task GivenNotExistingId_ReturnsFail(long id)
-//     {
-//         var result = await _sut.DeleteAsync(id);
-//
-//         result.IsFailed.Should().BeTrue();
-//     }
-//
-//     [Theory]
-//     [InlineData(-1)]
-//     [InlineData(5)]
-//     public async Task GivenNotExistingId_ReturnsUnknownInvalidIdError(long id)
-//     {
-//         var result = await _sut.DeleteAsync(id);
-//
-//         result.Errors.Should().ContainSingle(e => e is InvalidIdError);
-//     }
-//
-//     [Theory]
-//     [InlineData(1)]
-//     [InlineData(2)]
-//     public async Task GivenExistingId_ReturnsOk(long id)
-//     {
-//         var result = await _sut.DeleteAsync(id);
-//
-//         result.IsSuccess.Should().BeTrue();
-//     }
-//
-//     [Theory]
-//     [InlineData(1)]
-//     [InlineData(2)]
-//     public async Task GivenExistingId_DeletesTicket(long id)
-//     {
-//         var result = await _sut.DeleteAsync(id);
-//
-//         var ticket = await _defaultContext.Tickets.FindAsync(id);
-//         ticket.Should().BeNull();
-//     }
-//
-//     public async Task DisposeAsync()
-//     {
-//         var migrationContext = new DefaultContext(_classFixture.ConnectionString);
-//         await migrationContext.Database.EnsureDeletedAsync();
-//     }
-// }
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using o2rabbit.BizLog.Abstractions;
+using o2rabbit.BizLog.Context;
+using o2rabbit.BizLog.Options.TicketServiceContext;
+using o2rabbit.BizLog.Services;
+using o2rabbit.BizLog.Tests.AutoFixtureCustomization.TicketCustomizations;
+using o2rabbit.Core.Entities;
+using o2rabbit.Core.ResultErrors;
+using o2rabbit.Migrations.Context;
 
+namespace o2rabbit.BizLog.Tests.Services.WhenUsingTicketService;
+
+public class DeleteAsync : IAsyncLifetime, IClassFixture<TicketServiceClassFixture>
+{
+    private readonly TicketServiceClassFixture _classFixture;
+    private readonly DefaultContext _defaultContext;
+    private readonly Fixture _fixture;
+    private readonly TicketService _sut;
+    private readonly TicketServiceContext _ticketContext;
+    private readonly Mock<ITicketValidator> _ticketValidatorMock;
+
+    public DeleteAsync(TicketServiceClassFixture classFixture)
+    {
+        _classFixture = classFixture;
+        _defaultContext = new DefaultContext(_classFixture.ConnectionString);
+        _fixture = new Fixture();
+        _fixture.Customize(new TicketHasNoProcessNoParentsNoChildren());
+
+        _ticketContext =
+            new TicketServiceContext(
+                new OptionsWrapper<TicketServiceContextOptions>(new TicketServiceContextOptions()
+                {
+                    ConnectionString = _classFixture.ConnectionString!
+                }));
+
+        var loggerMock = new Mock<ILogger<TicketService>>();
+        _ticketValidatorMock = new Mock<ITicketValidator>();
+        _sut = new TicketService(_ticketContext, loggerMock.Object, _ticketValidatorMock.Object);
+    }
+
+    public async Task InitializeAsync()
+    {
+        var migrationContext = new DefaultContext(_classFixture.ConnectionString);
+        await migrationContext.Database.EnsureCreatedAsync();
+
+        var existingTicket = _fixture.Create<Ticket>();
+        var existingTicket2 = _fixture.Create<Ticket>();
+        existingTicket.Id = 1;
+        existingTicket2.Id = 2;
+
+        _defaultContext.Add(existingTicket);
+        _defaultContext.Add(existingTicket2);
+
+        await _defaultContext.SaveChangesAsync();
+
+        _defaultContext.Entry(existingTicket).State = EntityState.Detached;
+        _defaultContext.Entry(existingTicket2).State = EntityState.Detached;
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(5)]
+    public async Task GivenNotExistingId_ReturnsFail(long id)
+    {
+        var result = await _sut.DeleteAsync(id);
+
+        result.IsFailed.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(5)]
+    public async Task GivenNotExistingId_ReturnsUnknownInvalidIdError(long id)
+    {
+        var result = await _sut.DeleteAsync(id);
+
+        result.Errors.Should().ContainSingle(e => e is InvalidIdError);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task GivenExistingId_ReturnsOk(long id)
+    {
+        var result = await _sut.DeleteAsync(id);
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task GivenExistingId_DeletesTicket(long id)
+    {
+        var result = await _sut.DeleteAsync(id);
+
+        var ticket = await _defaultContext.Tickets.FindAsync(id);
+        ticket.Should().BeNull();
+    }
+
+    public async Task DisposeAsync()
+    {
+        var migrationContext = new DefaultContext(_classFixture.ConnectionString);
+        await migrationContext.Database.EnsureDeletedAsync();
+    }
+}
