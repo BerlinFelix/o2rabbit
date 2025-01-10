ea7d26c89e3ce5c1cc2b82ef4b958647a57b7a26
felix <haller.felix@outlook.de>
2025-01-10 09:35:37 +0100

    Add basic testing JWT authentication with login endpoint
    Implement a new `AuthController` with a `login` endpoint to generate JWT tokens using hardcoded credentials for testing purposes. Introduce `TokenDto` and `LoginContent` models to structure token responses and login data. Add dependency on `System.IdentityModel.Tokens.Jwt` for token generation.

---
+++ o2rabbit.Api/Models/TokenDto.cs
@@ -0,0 +1,6 @@
+namespace o2rabbit.Api.Models;
+
+public class TokenDto
+{
+    public required string Token { get; set; }
+}
---
+++ o2rabbit.Api/Options/LoginContent.cs
@@ -0,0 +1,8 @@
+namespace o2rabbit.Api.Options;
+
+public class LoginContent
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
