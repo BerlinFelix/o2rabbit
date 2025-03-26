30216a971a01e785d29d2ea6e6d254ca2a22f0a2
felix <haller.felix@outlook.de>
2025-03-26 15:16:47 +0100

    Refactor default entity creation into distinct methods
    Separated the creation of default entities (spaces, workflows, processes, statuses, and transitions) into modular, reusable private methods. Improves code readability, maintainability, and supports better future scalability.

--- o2rabbit.BizLog/Extensions/ContextExtensions.cs
+++ o2rabbit.BizLog/Extensions/ContextExtensions.cs
@@ -8,26 +8,89 @@ public static class ContextExtensions
     public static async Task AddAndSaveDefaultEntitiesAsync(this DefaultContext context,
         CancellationToken cancellationToken = default)
     {
-        var defaultSpace = new Space
+        await AddAndSaveDefaultSpace(context, cancellationToken).ConfigureAwait(false);
+        await AddAndSaveTodoProcess(context, cancellationToken).ConfigureAwait(false);
+        await AddAndSaveTodoWorkflow(context, cancellationToken).ConfigureAwait(false);
+        await AddAndSaveTaskProcess(context, cancellationToken).ConfigureAwait(false);
+        await AddAndSaveTaskWorkflow(context, cancellationToken).ConfigureAwait(false);
+    }
+
+    private static async Task AddAndSaveTaskWorkflow(DefaultContext context, CancellationToken cancellationToken)
+    {
+        var workflow = new Workflow
         {
-            Title = "Default",
-            Description = "This is the default space.",
-            Created = DateTimeOffset.UtcNow,
-            LastModified = DateTimeOffset.UtcNow
+            Name = "Task",
+            ProcessId = 1
         };
-        context.Spaces.Add(defaultSpace);
+        context.Workflows.Add(workflow);
         await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-        var todo = new Process
+        var openStatus = new Status
         {
-            Name = "Todo",
-            Description = "A simple todo",
-            WorkflowId = 1,
+            WorkflowId = workflow.Id,
+            Name = "Open"
         };
-        context.Processes.Add(todo);
+        var doneStatus = new Status
+        {
+            WorkflowId = workflow.Id,
+            Name = "Done",
+            IsFinal = true
+        };
+        var canceledStatus = new Status
+        {
+            WorkflowId = workflow.Id,
+            Name = "Canceled",
+            IsFinal = true
+        };
+        var inProgressStatus = new Status()
+        {
+            WorkflowId = workflow.Id,
+            Name = "In Progress",
+        };
+        context.Statuses.AddRange(openStatus, doneStatus, canceledStatus, inProgressStatus);
         await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        var openDoneTransition = new StatusTransition
+        {
+            Name = "Open to Done",
+            FromStatusId = openStatus.Id,
+            ToStatusId = doneStatus.Id
+        };
+        var openCancelTransition = new StatusTransition
+        {
+            Name = "Open to Canceled",
+            FromStatusId = openStatus.Id,
+            ToStatusId = canceledStatus.Id
+        };
+        var openInProgessTransition = new StatusTransition()
+        {
+            Name = "Open to In Progress",
+            FromStatusId = openStatus.Id,
+            ToStatusId = inProgressStatus.Id,
+        };
+        var inProgessCanceledTransition = new StatusTransition()
+        {
+            Name = "In Progress to Canceled",
+            FromStatusId = inProgressStatus.Id,
+            ToStatusId = canceledStatus.Id,
+        };
+        var inProgessDoneTransition = new StatusTransition()
+        {
+            Name = "In Progress to Done",
+            FromStatusId = inProgressStatus.Id,
+            ToStatusId = doneStatus.Id,
+        };
+        context.StatusTransitions.AddRange(
+            openDoneTransition, openCancelTransition, openInProgessTransition, inProgessCanceledTransition,
+            inProgessDoneTransition
+        );
+
+        await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private static async Task AddAndSaveTodoWorkflow(DefaultContext context, CancellationToken cancellationToken)
+    {
         var todoWorkflow = new Workflow
         {
-            Name = "TodoWorkflow",
+            Name = "Todo",
             ProcessId = 1
         };
         context.Workflows.Add(todoWorkflow);
@@ -67,6 +130,43 @@ public static class ContextExtensions
             openDoneTransition, openCancelTransition
         );
 
+        await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private static async Task AddAndSaveTaskProcess(DefaultContext context, CancellationToken cancellationToken)
+    {
+        var process = new Process
+        {
+            Name = "Task",
+            Description = "A task",
+            WorkflowId = 2
+        };
+        context.Processes.Add(process);
+        await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private static async Task AddAndSaveTodoProcess(DefaultContext context, CancellationToken cancellationToken)
+    {
+        var todo = new Process
+        {
+            Name = "Todo",
+            Description = "A simple todo",
+            WorkflowId = 1,
+        };
+        context.Processes.Add(todo);
+        await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private static async Task AddAndSaveDefaultSpace(DefaultContext context, CancellationToken cancellationToken)
+    {
+        var defaultSpace = new Space
+        {
+            Title = "Default",
+            Description = "This is the default space.",
+            Created = DateTimeOffset.UtcNow,
+            LastModified = DateTimeOffset.UtcNow
+        };
+        context.Spaces.Add(defaultSpace);
         await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
     }
 }
