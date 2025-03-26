817a76e671f12a07ce7b87e85baf127214d789ad
felix <haller.felix@outlook.de>
2025-03-26 10:47:39 +0100

    Add necessary migrations
    Introduces Workflow, Status, and StatusTransition entities to support process workflows. Updates the database schema and relationships accordingly, including a new WorkflowId column in the Processes table. Implements migrations to apply these schema changes.

---
+++ o2rabbit.Migrations/Migrations/20250326090544_AddWorkflowAndStatus.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace o2rabbit.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkflowAndStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkflowId",
+                table: "Processes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Workflows",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ProcessId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workflows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workflows_Processes_ProcessId",
+                        column: x => x.ProcessId,
+                        principalTable: "Processes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsFinal = table.Column<bool>(type: "boolean", nullable: false),
+                    WorkflowId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Statuses_Workflows_WorkflowId",
+                        column: x => x.WorkflowId,
+                        principalTable: "Workflows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusTransitions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    FromStatusId = table.Column<long>(type: "bigint", nullable: false),
+                    ToStatusId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusTransitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StatusTransitions_Statuses_FromStatusId",
+                        column: x => x.FromStatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StatusTransitions_Statuses_ToStatusId",
+                        column: x => x.ToStatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statuses_WorkflowId",
+                table: "Statuses",
+                column: "WorkflowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusTransitions_FromStatusId",
+                table: "StatusTransitions",
+                column: "FromStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusTransitions_ToStatusId",
+                table: "StatusTransitions",
+                column: "ToStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workflows_ProcessId",
+                table: "Workflows",
+                column: "ProcessId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusTransitions");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropTable(
+                name: "Workflows");
+
+            migrationBuilder.DropColumn(
+                name: "WorkflowId",
+                table: "Processes");
+        }
+    }
+}
