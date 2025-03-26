69d1ff2494b3900c5fd8a39fa5303671a859b3fd
felix <haller.felix@outlook.de>
2025-03-26 14:57:15 +0100

    Add necessay migrations

---
+++ o2rabbit.Migrations/Migrations/20250326103511_FixTypeInColumn.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace o2rabbit.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTypeInColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ProcessId",
+                table: "Tickets",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "WorkflowId",
+                table: "Processes",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ProcessId",
+                table: "Tickets",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkflowId",
+                table: "Processes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
